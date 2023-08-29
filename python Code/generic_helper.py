821ace39a58d52534695932160388b9c885eecf7
Didrikhya-Uzir <85503824+Didrikhya-Uzir@users.noreply.github.com>
2023-08-29 16:32:13 +0530

    Add files via upload

---
+++ python Code/generic_helper.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 28 09:03:53 2023
+
+@author: didri
+"""
+import re
+
+def get_str_from_food_dict(food_dict: dict):
+    result = ", ".join([f"{int(value)} {key}" for key, value in food_dict.items()])
+    return result
+
+
+def extract_session_id(session_str: str):
+    match = re.search(r"/sessions/(.*?)/contexts/", session_str)
+    if match:
+        extracted_string = match.group(0)
+        return extracted_string
+
+    return ""
